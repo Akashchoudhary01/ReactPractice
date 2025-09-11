3b954f0741bf938e6eb94930f62459188256efe0
Akash Choudhary <akashkrchoudhary007@gmail.com>
2025-09-11 22:38:12 +0530

    hahaha

---
+++ React-axios-Project/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className='bg-blue-600 h-lvh flex justify-center items-center text-4xl'>
+      Hello  , React 
+      
+    </div>
+  )
+}
